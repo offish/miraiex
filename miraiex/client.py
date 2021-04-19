60f9431023994e7bf10ef9c3bc14af28eda349a5
offish <overutilization@gmail.com>
2021-04-19 20:20:44 +0200

    Update client.py

--- miraiex/client.py
+++ miraiex/client.py
@@ -12,25 +12,25 @@ class MiraiEx:
     def get_tickers(self) -> dict:
         return self.get_markets()
 
-    @types(str)
+    @types(object, str)
     def get_market(self, market: str) -> dict:
         return get_request(MARKET, market)
 
     def get_prices(self, market: str) -> dict:
         return self.get_market(market)
 
-    @types(str, int)
+    @types(object, str, int)
     def get_market_history(self, market: str, count: int = 0) -> dict:
         payload = {"count": count} if count else {}
         return get_request(HISTORY, market, payload=payload)
 
-    @types(str)
+    @types(object, str)
     def get_orderbook(self, market: str) -> dict:
         return get_request(ORDERBOOKS, market)
 
     def get_orderbooks(self, market: str) -> dict:
         return self.get_orderbook(market)
 
-    @types(str)
+    @types(object, str)
     def get_market_ticker(self, market: str) -> dict:
         return get_request(TICKER, market)
